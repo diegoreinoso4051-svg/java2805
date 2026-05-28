eee3885893909dfb0b3e29f7cd5a90a995469a02
Diego Reinoso <diegoreinoso@MacBook-Air-de-Diego.local>
2026-05-28 15:14:26 -0500

    hola mundo

--- app/src/main/java/com/example/proyecto2805/App.java
+++ app/src/main/java/com/example/proyecto2805/App.java
@@ -5,7 +5,7 @@ package com.example.proyecto2805;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "HOLA DIEGO";
     }
 
     public static void main(String[] args) {
